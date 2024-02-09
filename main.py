659145b3f179775f9ae9bdb527bdd7450269341d
acsankar <acsankar@gmail.com>
2024-02-09 19:22:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,15 @@ import streamlit as st
 st.title('GPU Memory Requirement Calculator')
 
 # User inputs
-P = st.number_input('Enter the amount of parameters in the model (in billions):', min_value=0.0, value=7.0, step=0.1)
-Q = st.selectbox('Select the amount of bits used for loading the model:', options=[4, 8, 16, 32], index=2)
+#P = st.number_input('Enter the amount of parameters in the model (in billions):', min_value=0.0, value=7.0, step=0.1)
+P = st.number_input('Enter the amount of parameters in the model (in billions):', min_value=0.0, value=100.0, step=1)
+Q = st.selectbox('Select the amount of bits used for loading the model:', options=[1,2,3,4,5,6,7 8, 16, 32], index=2)
 
 # Calculate GPU memory (M)
 # Convert P from billions to actual number of parameters
 P = P * 1e9  # Convert billions to actual number
-M = (((32 / Q) * (P * 4)) / 32) * 1.2
+#M = (((32 / Q) * (P * 4)) / 32) * 1.2
+M = ((P * 4)/(32 / Q)) * 1.2
 
 # Display the result
 st.write(f'GPU memory required: {M / 1e9:.2f} GB')
