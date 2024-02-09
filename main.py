3d7f908c7593aa2b3e2b8b1bdea8e927324425ae
acsankar <acsankar@gmail.com>
2024-02-09 19:23:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ st.title('GPU Memory Requirement Calculator')
 # User inputs
 #P = st.number_input('Enter the amount of parameters in the model (in billions):', min_value=0.0, value=7.0, step=0.1)
 P = st.number_input('Enter the amount of parameters in the model (in billions):', min_value=0.0, value=100.0, step=1)
-Q = st.selectbox('Select the amount of bits used for loading the model:', options=[1, 2, 3, 4, 5, 6, 7 8, 16, 32], index=1)
+Q = st.selectbox('Select the amount of bits used for loading the model:', options=[1, 2, 3, 4, 5, 6, 7, 8, 16, 32], index=1)
 
 # Calculate GPU memory (M)
 # Convert P from billions to actual number of parameters
