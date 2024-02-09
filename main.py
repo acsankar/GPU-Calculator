c74788fa264980755350b9b56d8635f35561da26
acsankar <acsankar@gmail.com>
2024-02-09 19:55:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 import streamlit as st
 
 # Streamlit app title
-st.title('GPU Memory Requirement Calculator')
+st.title('LLM - GPU Memory Calculator')
 
 # User inputs
-#P = st.number_input('Enter the amount of parameters in the model (in billions):', min_value=0.0, value=7.0, step=0.1)
-P = st.number_input('Enter the amount of parameters in the model (in billions):', min_value=0.0, value=100.0, step=1)
-Q = st.selectbox('Select the amount of bits used for loading the model:', options=[1, 2, 3, 4, 5, 6, 7, 8, 16, 32], index=1)
+P = 0
+
+P = st.number_input('Enter the amount of parameters in the LLM (in billions):', min_value=1.0, value=7.0, step=1.0)
+st.caption('*** Enter 30 for 30 Billion parameter LLM')
+Q = st.number_input('Select the amount of bits used for loading the model:', min_value=2.0, value=32.0, step=1.0)
+st.caption('*** Most of the models are full 32 bits. If you are using quantized version enter the bits (Enter 5 for 5 bit Q)')
+
 
 # Calculate GPU memory (M)
 # Convert P from billions to actual number of parameters
 P = P * 1e9  # Convert billions to actual number
-#M = (((32 / Q) * (P * 4)) / 32) * 1.2
-M = ((P * 4)/(32 / Q)) * 1.2
+
+# Corrected formula
+M = ((P * 4) / (32 / Q)) * 1.2
 
 # Display the result
-st.write(f'GPU memory required: {M / 1e9:.2f} GB')
+st.header(f'GPU memory required : {M / 1e9:.2f} GB')
